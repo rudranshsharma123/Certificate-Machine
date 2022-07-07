3ac40c15abfd647db9f94cb8bf26b22ebb711b8b
rudransh <rudranshsharma123@gmail.com>
2022-07-07 17:39:13 +0530

    added the app which mints and sends nft

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,10 @@
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 import Papa from 'papaparse'
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import idl from './assets/idl.json'
+import { Connection, PublicKey, clusterApiUrl, Keypair } from '@solana/web3.js';
+import { AnchorProvider, Program, Provider, web3, utils } from '@project-serum/anchor';
 
 // Constants
 const TWITTER_HANDLE = '_buildspace';
@@ -13,12 +16,203 @@ const App = () => {
   // It state will contain the error when
   // correct file extension is not used
   const [error, setError] = useState("");
-
+  const [walletAddress, setWalletAddress] = useState(null);
   // It will store the file uploaded by the user
   const [file, setFile] = useState("");
+  const network = clusterApiUrl('devnet');
+  const testNftUri = "https://raw.githubusercontent.com/rudranshsharma123/Certificate-Machine/main/test.json"
+  const TOKEN_METADATA_PROGRAM_ID = new web3.PublicKey(
+    "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s",
+  );
+  // Controls how we want to acknowledge when a transaction is "done".
+  const opts = {
+    preflightCommitment: "processed"
+  }
+  const programID = new PublicKey(idl.metadata.address);
 
+  const testNftTitle = "Cukc";
+  const testNftSymbol = "CUCK";
+  const CREATE_MINT_SEED = "createmints";
+
+  // Only call this function once for each wallet address no need to run it again it will fail
+  const initializeStorageAccount = async () => {
+    const baseAccount = new PublicKey(walletAddress)
+    const provider = getProvider();
+    const program = new Program(idl, programID, provider);
+    const [pda, _] = await PublicKey.findProgramAddress(
+      [
+        baseAccount.toBuffer(),
+        Buffer.from(utils.bytes.utf8.encode(CREATE_MINT_SEED)),
+      ],
+      program.programId,
+    );
+    const txn = await program.methods
+      .initializeStorageAccount()
+      .accounts({
+        storageAccount: pda,
+      })
+      .rpc();
+  }
+
+  const getGifList = async (buyer_address, name_of_nft, symbol_of_nft, metadata_uri) => {
+    try {
+
+      const buyer = new PublicKey(buyer_address)
+      const baseAccount = new PublicKey(walletAddress)
+      console.log(baseAccount)
+      const provider = getProvider();
+      const program = new Program(idl, programID, provider);
+      // const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+      const mint = Keypair.generate()
+
+      const tokenAddress = await utils.token.associatedAddress({
+        mint: mint.publicKey,
+        owner: baseAccount,
+      });
+      const [pda, _] = await PublicKey.findProgramAddress(
+        [
+          baseAccount.toBuffer(),
+          Buffer.from(utils.bytes.utf8.encode(CREATE_MINT_SEED)),
+        ],
+        program.programId,
+      );
+      // const txn = await program.methods
+      //   .initializeStorageAccount()
+      //   .accounts({
+      //     storageAccount: pda,
+      //   })
+      //   .rpc();
+      console.log(`New token: ${mint.publicKey}`);
+      const metadataAddress = (
+        await web3.PublicKey.findProgramAddress(
+          [
+            Buffer.from("metadata"),
+            TOKEN_METADATA_PROGRAM_ID.toBuffer(),
+            mint.publicKey.toBuffer(),
+          ],
+          TOKEN_METADATA_PROGRAM_ID,
+        )
+      )[0];
+      console.log("Metadata initialized");
+      const masterEditionAddress = (
+        await web3.PublicKey.findProgramAddress(
+          [
+            Buffer.from("metadata"),
+            TOKEN_METADATA_PROGRAM_ID.toBuffer(),
+            mint.publicKey.toBuffer(),
+            Buffer.from("edition"),
+          ],
+          TOKEN_METADATA_PROGRAM_ID,
+        )
+      )[0];
+      console.log("Master edition metadata initialized");
+      const tx = await program.methods
+        .mint(name_of_nft, symbol_of_nft, metadata_uri, baseAccount.publicKey)
+        .accounts({
+          storageAccount: pda,
+          masterEdition: masterEditionAddress,
+          metadata: metadataAddress,
+          mint: mint.publicKey,
+          tokenAccount: tokenAddress,
+          mintAuthority: baseAccount,
+          tokenMetadataProgram: TOKEN_METADATA_PROGRAM_ID,
+        })
+        .signers([mint])
+        .rpc();
+      console.log("Your transaction signature", tx);
+
+      // console.log("Got the account", account)
+
+      const ownerTokenAddress = await utils.token.associatedAddress({
+        mint: mint.publicKey,
+        owner: baseAccount,
+      });
+      const buyerTokenAddress = await utils.token.associatedAddress({
+        mint: mint.publicKey,
+        owner: buyer,
+      });
+      // console.log(`Request to sell NFT: ${mint} for ${saleAmount} lamports.`);
+      console.log(`Owner's Token Address: ${ownerTokenAddress}`);
+      console.log(`Buyer's Token Address: ${buyerTokenAddress}`);
+
+      // Transact with the "sell" function in our on-chain program
+
+      await program.methods
+        .send()
+        .accounts({
+          mint: mint.publicKey,
+          ownerTokenAccount: ownerTokenAddress,
+          ownerAuthority: baseAccount,
+          buyerTokenAccount: buyerTokenAddress,
+          buyerAuthority: buyer,
+        })
+        .rpc();
+    } catch (error) {
+      console.log("Error in getGifList: ", error)
+
+    }
+  }
   // This function will be called when
   // the file input changes
+  const { SystemProgram, Keypair } = web3;
+  const getProvider = () => {
+    const connection = new Connection(network, opts.preflightCommitment);
+    const provider = new AnchorProvider(
+      connection, window.solana, opts.preflightCommitment,
+    );
+    return provider;
+  }
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
+
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log('Phantom wallet found!');
+          const response = await solana.connect({ onlyIfTrusted: true });
+          console.log(
+            'Connected with Public Key:',
+            response.publicKey.toString()
+
+          );
+          setWalletAddress(response.publicKey.toString());
+        }
+      } else {
+        alert('Solana object not found! Get a Phantom Wallet 👻');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const connectWallet = async () => {
+    const { solana } = window;
+
+    if (solana) {
+      const response = await solana.connect();
+      console.log('Connected with Public Key:', response.publicKey.toString());
+      setWalletAddress(response.publicKey.toString());
+    }
+  };
+  const renderNotConnectedContainer = () => (
+    <button
+      className="cta-button connect-wallet-button"
+      onClick={connectWallet}
+    >
+      Connect to Wallet
+    </button>
+  );
+
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad);
+  }, []);
+
+
+
+
   const handleFileChange = (e) => {
     setError("");
 
@@ -54,6 +248,7 @@ const App = () => {
     reader.onload = async ({ target }) => {
       const csv = Papa.parse(target.result, { header: true });
       const parsedData = csv?.data;
+      console.log(parsedData)
       const columns = Object.keys(parsedData[0]);
       setData(columns);
     };
@@ -64,12 +259,19 @@ const App = () => {
 
   return (
     <div className="App">
-      <div className="container">
+      <div className={walletAddress ? 'authed-container' : 'container'}>
+
         <div className="header-container">
           <p className="header">🖼 GIF Portal</p>
           <p className="sub-text">
             View your GIF collection in the metaverse ✨
           </p>
+        </div>
+        <div>{!walletAddress && renderNotConnectedContainer()}</div>
+        <div>
+          <button onClick={async () => {
+            getGifList("8QCJWmu1gErc9zRFtww9Bo4R7bne58YGvL4YZRGbSeX9", testNftTitle, testNftSymbol, testNftUri)
+          }}>hello</button>
         </div>
         <div>
           <label htmlFor="csvInput" style={{ display: "block" }}>
